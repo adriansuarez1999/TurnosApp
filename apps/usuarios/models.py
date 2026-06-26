c4c1e48931cf4cd0df3e6013db46a858202a688d
Adrian <feradrian1999@gmail.com>
2026-06-26 03:47:02 -0300

    Se agrega base de datos y modelos nuevos, se corrigió view.py para que la función de creación de usuarios coincidiera con los campos de la BD

--- apps/usuarios/models.py
+++ apps/usuarios/models.py
@@ -1,149 +1,184 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-
-
-class Usuario(models.Model):
-    nombre = models.CharField(max_length=100)
-    apellido = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    telefono = models.CharField(max_length=20, blank=True)
-    password = models.CharField(max_length=255)
-
-    class Meta:
-        db_table = 'Cliente'
-
-    def __str__(self):
-        return self.email
 
 
 class Barberia(models.Model):
     id_barberia = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=25)
-    duenio = models.CharField(max_length=30)
-    direccion = models.CharField(max_length=60)
+    id_dueno = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='id_dueno')
+    nombre = models.CharField(max_length=150)
+    direccion = models.CharField(max_length=200)
+    zona = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+    logo = models.CharField(max_length=255, blank=True, null=True)
+    telefono = models.CharField(max_length=20, blank=True, null=True)
+    estado = models.CharField(max_length=8)
 
     class Meta:
-        db_table = 'Barberia'
+        managed = False
+        db_table = 'barberia'
 
-    def __str__(self):
-        return self.nombre
+
+class Barbero(models.Model):
+    id_barbero = models.AutoField(primary_key=True)
+    id_usuario = models.OneToOneField('Usuario', models.DO_NOTHING, db_column='id_usuario')
+    id_barberia = models.ForeignKey(Barberia, models.DO_NOTHING, db_column='id_barberia')
+    especialidad = models.CharField(max_length=100, blank=True, null=True)
+    promedio_calificacion = models.DecimalField(max_digits=3, decimal_places=2)
+
+    class Meta:
+        managed = False
+        db_table = 'barbero'
+
+
+class Calificacion(models.Model):
+    id_calificacion = models.AutoField(primary_key=True)
+    id_turno = models.OneToOneField('Turno', models.DO_NOTHING, db_column='id_turno')
+    id_barbero = models.ForeignKey(Barbero, models.DO_NOTHING, db_column='id_barbero')
+    id_cliente = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='id_cliente')
+    puntuacion = models.IntegerField()
+    comentario = models.TextField(blank=True, null=True)
+    fecha = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'calificacion'
+
+
+class Disponibilidad(models.Model):
+    id_disponibilidad = models.AutoField(primary_key=True)
+    id_barbero = models.ForeignKey(Barbero, models.DO_NOTHING, db_column='id_barbero')
+    dia_semana = models.CharField(max_length=3)
+    hora_inicio = models.TimeField()
+    hora_fin = models.TimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'disponibilidad'
+
+
+class Notificacion(models.Model):
+    id_notificacion = models.AutoField(primary_key=True)
+    id_usuario = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='id_usuario')
+    tipo = models.CharField(max_length=50)
+    mensaje = models.TextField()
+    fecha_envio = models.DateTimeField()
+    estado = models.CharField(max_length=9)
+
+    class Meta:
+        managed = False
+        db_table = 'notificacion'
+
+
+class Pago(models.Model):
+    id_pago = models.AutoField(primary_key=True)
+    id_turno = models.ForeignKey('Turno', models.DO_NOTHING, db_column='id_turno')
+    fecha_pago = models.DateTimeField()
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+    metodo_pago = models.CharField(max_length=13)
+    estado = models.CharField(max_length=9)
+    tipo_pago = models.CharField(max_length=7)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    created_by = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='created_by', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'pago'
+
+
+class PlanSuscripcion(models.Model):
+    id_plan = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    incluye_estadisticas = models.IntegerField()
+    incluye_promociones = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'plan_suscripcion'
+
+
+class Promocion(models.Model):
+    id_promocion = models.AutoField(primary_key=True)
+    id_servicio = models.ForeignKey('Servicio', models.DO_NOTHING, db_column='id_servicio')
+    nombre = models.CharField(max_length=100)
+    porcentaje_descuento = models.DecimalField(max_digits=5, decimal_places=2)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    activa = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'promocion'
 
 
 class Servicio(models.Model):
     id_servicio = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=45)
+    id_barberia = models.ForeignKey(Barberia, models.DO_NOTHING, db_column='id_barberia')
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
-    descripcion = models.CharField(max_length=100, blank=True, null=True)
+    duracion_minutos = models.IntegerField()
+    estado = models.IntegerField()
 
     class Meta:
-        db_table = 'Servicio'
-
-    def __str__(self):
-        return self.nombre
+        managed = False
+        db_table = 'servicio'
 
 
-class Profesionales(models.Model):
-    id_profesional = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=45)
-    apellido = models.CharField(max_length=45)
-    edad = models.IntegerField(blank=True, null=True)
-    red_social = models.CharField(max_length=45, blank=True, null=True)
-    telefono = models.CharField(max_length=20, blank=True, null=True)
-    id_barberia = models.ForeignKey(
-        Barberia,
-        on_delete=models.PROTECT,
-        db_column='id_barberia'
-    )
+class Suscripcion(models.Model):
+    id_suscripcion = models.AutoField(primary_key=True)
+    id_barberia = models.ForeignKey(Barberia, models.DO_NOTHING, db_column='id_barberia')
+    id_plan = models.ForeignKey(PlanSuscripcion, models.DO_NOTHING, db_column='id_plan')
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    estado = models.CharField(max_length=7)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    created_by = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='created_by', blank=True, null=True)
 
     class Meta:
-        db_table = 'Profesionales'
-
-    def __str__(self):
-        return f'{self.nombre} {self.apellido}'
+        managed = False
+        db_table = 'suscripcion'
 
 
 class Turno(models.Model):
-    ESTADO_CHOICES = [
-        ('libre', 'Libre'),
-        ('reservado', 'Reservado'),
-        ('terminado', 'Terminado'),
-        ('cancelado', 'Cancelado'),
-    ]
-
     id_turno = models.AutoField(primary_key=True)
-    id_cliente = models.ForeignKey(
-        Usuario,
-        on_delete=models.PROTECT,
-        db_column='id_cliente'
-    )
-    id_profesional = models.ForeignKey(
-        Profesionales,
-        on_delete=models.PROTECT,
-        db_column='id_profesional'
-    )
+    id_cliente = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='id_cliente')
+    id_barbero = models.ForeignKey(Barbero, models.DO_NOTHING, db_column='id_barbero')
+    id_servicio = models.ForeignKey(Servicio, models.DO_NOTHING, db_column='id_servicio')
     fecha = models.DateField()
     hora_inicio = models.TimeField()
     hora_fin = models.TimeField()
-    estado = models.CharField(max_length=10, choices=ESTADO_CHOICES, default='libre')
+    estado = models.CharField(max_length=10)
     monto_total = models.DecimalField(max_digits=10, decimal_places=2)
-    creado_el = models.DateTimeField(auto_now_add=True)
-    id_barberia = models.ForeignKey(
-        Barberia,
-        on_delete=models.PROTECT,
-        db_column='id_barberia'
-    )
-    monto_propina = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    metodo_pago = models.CharField(max_length=20)
-    id_servicio = models.ForeignKey(
-        Servicio,
-        on_delete=models.PROTECT,
-        db_column='id_servicio'
-    )
+    fecha_reserva = models.DateTimeField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    created_by = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='created_by', related_name='turno_created_by_set', blank=True, null=True)
 
     class Meta:
-        db_table = 'Turno'
-
-    def __str__(self):
-        return f'Turno {self.id_turno} - {self.fecha}'
+        managed = False
+        db_table = 'turno'
 
 
-class Calificaciones(models.Model):
-    id_calificaciones = models.AutoField(primary_key=True)
-    id_cliente = models.ForeignKey(
-        Usuario,
-        on_delete=models.PROTECT,
-        db_column='id_cliente'
-    )
-    id_profesional = models.ForeignKey(
-        Profesionales,
-        on_delete=models.PROTECT,
-        db_column='id_profesional'
-    )
-    descripcion = models.CharField(max_length=150, blank=True, null=True)
-    fecha_hora = models.DateTimeField(auto_now_add=True)
-    puntaje = models.PositiveSmallIntegerField()
+class Usuario(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.CharField(unique=True, max_length=150)
+    telefono = models.CharField(max_length=20)
+    password = models.CharField(max_length=255)
+    rol = models.CharField(max_length=7)
+    estado = models.CharField(max_length=9)
+    fecha_registro = models.DateTimeField()
 
     class Meta:
-        db_table = 'Calificaciones'
-
-    def __str__(self):
-        return f'Calificacion {self.id_calificaciones} - puntaje {self.puntaje}'
-
-
-class BarberiaServicio(models.Model):
-    id_barberia_servicio = models.AutoField(primary_key=True)
-    id_barberia = models.ForeignKey(
-        Barberia,
-        on_delete=models.PROTECT,
-        db_column='id_barberia'
-    )
-    id_servicio = models.ForeignKey(
-        Servicio,
-        on_delete=models.PROTECT,
-        db_column='id_servicio'
-    )
-
-    class Meta:
-        db_table = 'Barberia_Servicio'
-
-    def __str__(self):
-        return f'Barberia {self.id_barberia_id} - Servicio {self.id_servicio_id}'
+        managed = False
+        db_table = 'usuario'
